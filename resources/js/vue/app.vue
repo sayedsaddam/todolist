98b4e1fa14955c04de23525110ff340448e8dc98
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-08-25 00:57:21 +0500

    Modified 12 files including commons, components and vue file. Added some js and css on to the pages.

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form />
+        </div>
+        <list-view />
+    </div>
+</template>
+<script>
+import addItemFrom from './addItemForm';
+import listView from './listView';
+    export default{
+        components: {
+            addItemFrom,
+            listView
+        }
+    }
+</script>
+
+<style scoped>
+    .todoListContainer{
+        width: 350px;
+        margin: auto;
+    }
+    .heading{
+        background: #e6e6e6;
+        padding: 10px;
+    }
+    #title{
+        text-align: center;
+    }
+</style>
